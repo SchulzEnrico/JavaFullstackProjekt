e6de55776e77efb214fff7d5ae96dc3406787579
Enrico Schulz <107748954+SchulzEnrico@users.noreply.github.com>
2023-09-22 13:18:34 +0200

    feat FigletFont

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import com.github.lalyos.jfiglet.JFiglet;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        String asciiArt = FigletFont.convertOneLine("Hello World");
+        System.out.println((asciiArt));
     }
 }
